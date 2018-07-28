7f8e08135c7859a143b215afffe1a4a03b2edc9f
Yang Zhang <yang.zhang@icloud.com>
2018-07-29 08:41:33 +1000

    Add project files.

---
+++ quartznet-database-demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace quartznet_database_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
